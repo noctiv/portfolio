1a8e718fe8e7d041592e581758bb12a3ba3ac2e7
unknown <noctiv>
2020-09-10 22:28:59 +0530

    Updates

---
+++ precache-manifest.994ab1d72c9e93b88c2b9d0a4a18021e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "517011caeaba31fb0c2d",
+    "url": "/portfolio/static/css/main.d280f9e9.chunk.css"
+  },
+  {
+    "revision": "517011caeaba31fb0c2d",
+    "url": "/portfolio/static/js/main.517011ca.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "383f08faaadf590177b187e095bd7144",
+    "url": "/portfolio/index.html"
+  }
+];
